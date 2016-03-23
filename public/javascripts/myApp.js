c58ee6f06f0ceb764f174e8603c8cc0e499d2c66
Ziteng Zhang <you@example.com>
2016-03-23 01:43:55 -0700

    refactor client side code
    make employee list and new employee work

--- public/javascripts/myApp.js
+++ public/javascripts/myApp.js
@@ -1,183 +1,60 @@
-(function(){
-    angular.module('myApp', ['ngRoute'])
-        //3 views routing
-    .config(['$routeProvider',
-        function($routeProvider) {
-            $routeProvider
-                .when('/', {
-                    templateUrl : 'templates/user_list.html',
-                    controller : 'userCtrl'
-                })
-                .when('/edit_user/:param', {
-                    templateUrl : 'templates/edit_user.html',
-                    controller:'EdituserController'
-                })
-                .when('/new_user', {
-                    templateUrl :'templates/new_user.html',
-                    controller : 'NewuserController'
-                })
-                .when('/new_employee', {
-                    templateUrl :'templates/newEmpolyee.html',
-                    controller : 'NewEmployeeCtrl'
-                })
-                .otherwise({
-                    redirectTo :  '/'
-                });
-        }])
-    //user data factory
-    .factory('userFactory', ['$http',function($http) {
-        return {
-            getUsers : function() {
-                return $http.get('/users');
-            },
-            deleteUser : function(id) {
-                return $http.delete('/users/'+id.toString());
-            },
-            updateUser : function(userdata) {
-                return $http.put('/users/'+userdata._id.toString(),userdata);
-            },
-            addUser : function(userdata) {
-                return $http.post('/users',userdata);
-            }
-        };
+angular.module('myApp', ['ngRoute'])
+//routing config
+.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl : 'templates/employee_list.html',
+                controller : 'EmployeeListCtrl'
+            })
+            .when('/new_employee', {
+                templateUrl :'templates/newEmpolyee.html',
+                controller : 'NewEmployeeCtrl'
+            })
+            .otherwise({
+                redirectTo :  '/'
+            });
     }])
-        .controller('NewEmployeeCtrl', function($scope) {
-
-
-            $scope.addEmployee = function() {
-                var employee = {
-                    fName : $scope.fName,
-                    lName : $scope.lName,
-                    title : $scope.title,
-                    sex   : $scope.sex,
-                    startDate : $scope.startDate,
-                    officePhone : $scope.officePhone,
-                    cellPhone : $scope.cellPhone,
-                    email : $scope.email,
-                    manager : $scope.manager,
-                    directReports : $scope.directReports
-                };
-                console.log(employee);
-            }
-        })
-    //User list controller
-    .controller('userCtrl', function($scope,userFactory) {
-        //Initialize users data
-        function getMyUser() {
-            userFactory.getUsers()
-                .then(function(res) {
-                    $scope.users = res.data;
-                    //console.log($scope.users);
-                });
+//employee data factory
+.factory('employeeFactory', ['$http',function($http) {
+    return {
+        getEmployees : function() {
+            return $http.get('/employees');
+        },
+        addEmployee : function(data) {
+            return $http.post('/employees',data);
         }
-        getMyUser();
-
-        $scope.deleteUser = function(id) {
-            userFactory.deleteUser(id)
-                .then(function(res){
-                    console.log(res);
-                    getMyUser();
-                });
-
+    };
+}])
+//new employee controller
+.controller('NewEmployeeCtrl', function($scope, $location, employeeFactory) {
+    $scope.addEmployee = function() {
+        var employee = {
+            name : $scope.name,
+            title : $scope.title,
+            sex   : $scope.sex,
+            startDate : $scope.startDate,
+            officePhone : $scope.officePhone,
+            cellPhone : $scope.cellPhone,
+            email : $scope.email,
+            manager : $scope.manager,
         };
-        $scope.orderByMe = function(x) {
-            $scope.myOrderBy = x;
-        };
+        employeeFactory.addEmployee(employee);
+        $location.path("#/");
+    }
+})
+//employee list controller
+.controller('EmployeeListCtrl', function($scope,employeeFactory) {
+    function getEmployees() {
+        employeeFactory.getEmployees()
+            .then(function(res) {
+                $scope.employees = res.data;
+            });
+    }
+    getEmployees();
 
-    })
-    //Edit User View Controller
-    .controller('EdituserController', function($scope,$location,$routeParams,userFactory) {
-        $scope.id = $routeParams.param;
-        var index = 0;
-        function getMyUser() {
-            userFactory.getUsers()
-                .then(function(res) {
-                    $scope.users = res.data;
-                    //console.log($scope.users);
-                    //find user obj by id
+    $scope.orderByMe = function(x) {
+        $scope.myOrderBy = x;
+    };
 
-                    for(index; index < $scope.users.length; index++) {
-                        if($scope.id == $scope.users[index]._id) break;
-                    }
-
-                    $scope.fName = $scope.users[index].fName;
-                    $scope.lName = $scope.users[index].lName;
-                    $scope.passw1 = $scope.users[index].passw1;
-                    $scope.passw2 = $scope.users[index].passw2;
-                    $scope.title = $scope.users[index].title;
-                    $scope.sex = $scope.users[index].sex;
-                    $scope.age = $scope.users[index].age;
-                });
-        }
-        getMyUser();
-
-        $scope.updateUser = function() {
-            $scope.users[index].fName = $scope.fName;
-            $scope.users[index].lName = $scope.lName;
-            $scope.users[index].passw1 = $scope.passw1;
-            $scope.users[index].passw2 = $scope.passw2;
-            $scope.users[index].title = $scope.title;
-            $scope.users[index].sex = $scope.sex;
-            $scope.users[index].age = $scope.age;
-            var userdataObj =  $scope.users[index];
-            userFactory.updateUser(userdataObj)
-                .then(function(res){
-                    console.log(res);
-                    //getMyUser();
-                    $location.path("#/user_list");
-                });
-
-        };
-
-    })
-    //New User View Controller
-    .controller('NewuserController', function($scope,$location,userFactory) {
-        $scope.fName = '';
-        $scope.lName = '';
-        $scope.passw1 = '';
-        $scope.passw2 = '';
-        $scope.title = '';
-        $scope.sex = '';
-        $scope.age = '';
-        $scope.error = false;
-        $scope.incomplete = false;
-        function getMyUser() {
-            userFactory.getUsers()
-                .then(function(res) {
-                    $scope.users = res.data;
-                    //console.log($scope.users);
-                });
-        }
-        getMyUser();
-
-        $scope.addUser = function() {
-            userFactory.addUser({fName:$scope.fName, lName: $scope.lName,
-                   title:$scope.title, sex : $scope.sex, age:$scope.age})
-                .then(function(res){
-                    $location.path("#/user_list");
-                });
-
-        };
-        $scope.$watch('passw1',function() {$scope.test();});
-        $scope.$watch('passw2',function() {$scope.test();});
-        $scope.$watch('fName', function() {$scope.test();});
-        $scope.$watch('lName', function() {$scope.test();});
-        $scope.$watch('title', function() {$scope.test();});
-        $scope.$watch('sex', function() {$scope.test();});
-        $scope.$watch('age', function() {$scope.test();});
-        $scope.test = function() {
-            if ($scope.passw1 !== $scope.passw2) {
-                $scope.error = true;
-            } else {
-                $scope.error = false;
-            }
-            $scope.incomplete = false;
-            if  (!$scope.fName.length ||
-                !$scope.lName.length ||
-                !$scope.passw1.length || !$scope.passw2.length
-                ||!$scope.title.length||!$scope.sex.length||!$scope.age.length) {
-                $scope.incomplete = true;
-            }
-        };
-    });
-})();
+});
